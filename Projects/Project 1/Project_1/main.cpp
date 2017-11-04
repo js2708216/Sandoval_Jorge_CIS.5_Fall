41ee5b3e27909cceb1c17f1335095e8fd9ed1658
js2708216 <32681604+js2708216@users.noreply.github.com>
2017-11-04 01:18:25 -0700

    P1

---
+++ Projects/Project 1/Project_1/main.cpp
@@ -0,0 +1,53 @@
+/* 
+ * File:   main.cpp
+ * Author: Jorge Sandoval
+ * Created on October 26th, 2017
+ * Purpose:  BlackJack
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+
+string n;
+string pCO;
+string pCT;
+string pCTee;
+string pCF;
+  
+    //Input or initialize values Here
+    
+//Start
+
+cout << "What is your Name?" << endl;
+cin >> n;
+cout << "" << endl;
+
+cout<<"Welcome to Blackjack,"<<n<<". I am your dealer today. Here Are some Rules." << endl;
+cout << "\n" << endl;
+cout << "                          Rules of Blackjack" << endl;
+cout << "                                    Rules:                               " << endl;
+
+cout << "" << endl;
+
+
+
+  
+    
+    //Process/Calculations Here
+
+  
+    //Exit
+    return 0;
+}
